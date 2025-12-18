d04f862d5fa4232d193145115cc82f03199294a1
Office Development <developmentoffice71@gmail.com>
2025-12-18 16:40:17 +0530

    modified:   Services/loginServices.js

--- Services/loginServices.js
+++ Services/loginServices.js
@@ -6,75 +6,78 @@ const PLSDBM82 = definePLSDBM82(sequelizeSDB);
 const PLSDBCMP = definePLSDBCMP(sequelizeSDB);
 
 class AuthenticationService {
-    constructor(corporateID, userUnq) {
-        this.corporateID = corporateID
-        this.uniqId = userUnq
-    }
-  
-    async authenticateUser() {
-      try {
-         
-        let resultM82 = await PLSDBM82.findAll({
-            attributes:['M82F01', 'M82F02', 'M82CMP'],
-            where: {M82F01 : this.uniqId}
-        }) 
-        let defaultCompany = null;
-        const CompList = [];
-  
-        if (resultM82.length === 0) {
-          console.error('No user-company relationships found.');
-        } else {
-          const userDataList = resultM82.map(record => ({
-            UId: record.M82F01,
-            GId: record.M82F02,
-            isDefault: record.M82CMP === 'Y'
-          }));
-  
-          for (const userData of userDataList) {
-            const { GId, isDefault } = userData;
-  
-            try {
-              const resultCMP = await PLSDBCMP.findAll({
-                attributes: ['CMPF01', 'CMPF02', 'CMPF04'],
-                where: {CMPF01 : GId}
-              })
-              if (resultCMP.length > 0) {
-                const groupDetails = resultCMP[0];
-  
-                if (isDefault && !defaultCompany) {
-                  defaultCompany = {
-                    cmpNo: groupDetails.CMPF01,
-                    cmpName: groupDetails.CMPF02,
-                    cmpGrp: groupDetails.CMPF04
-                  };
-                }
-  
-                CompList.push({
+  constructor(corporateID, userUnq) {
+    this.corporateID = corporateID
+    this.uniqId = userUnq
+  }
+
+  async authenticateUser() {
+    try {
+
+      let resultM82 = await PLSDBM82.findAll({
+        attributes: ['M82F01', 'M82F02', 'M82CMP'],
+        where: {
+          M82F01: this.uniqId,
+          M82ADA: 'A'
+        }
+      })
+      let defaultCompany = null;
+      const CompList = [];
+
+      if (resultM82.length === 0) {
+        console.error('No user-company relationships found.');
+      } else {
+        const userDataList = resultM82.map(record => ({
+          UId: record.M82F01,
+          GId: record.M82F02,
+          isDefault: record.M82CMP === 'Y'
+        }));
+
+        for (const userData of userDataList) {
+          const { GId, isDefault } = userData;
+
+          try {
+            const resultCMP = await PLSDBCMP.findAll({
+              attributes: ['CMPF01', 'CMPF02', 'CMPF04'],
+              where: { CMPF01: GId }
+            })
+            if (resultCMP.length > 0) {
+              const groupDetails = resultCMP[0];
+
+              if (isDefault && !defaultCompany) {
+                defaultCompany = {
                   cmpNo: groupDetails.CMPF01,
                   cmpName: groupDetails.CMPF02,
                   cmpGrp: groupDetails.CMPF04
-                });
-  
-              } else {
-                console.error(`No details found for Group ID: ${GId}`);
+                };
               }
-              CompList.sort((a, b) => parseInt(a.cmpNo) - parseInt(b.cmpNo));
 
-            } catch (err) {
-              console.error(`Error fetching details for Group ID ${GId}:` + err.message);
+              CompList.push({
+                cmpNo: groupDetails.CMPF01,
+                cmpName: groupDetails.CMPF02,
+                cmpGrp: groupDetails.CMPF04
+              });
+
+            } else {
+              console.error(`No details found for Group ID: ${GId}`);
             }
+            CompList.sort((a, b) => parseInt(a.cmpNo) - parseInt(b.cmpNo));
+
+          } catch (err) {
+            console.error(`Error fetching details for Group ID ${GId}:` + err.message);
           }
         }
-  
-        return {
-          DefComp: defaultCompany,
-          CompList,
-        };
-      } catch (err) {
-        console.error(err);
-        return { success: false, message: 'Internal server error' };
       }
+
+      return {
+        DefComp: defaultCompany,
+        CompList,
+      };
+    } catch (err) {
+      console.error(err);
+      return { success: false, message: 'Internal server error' };
     }
   }
-  
-  module.exports = AuthenticationService
+}
+
+module.exports = AuthenticationService
