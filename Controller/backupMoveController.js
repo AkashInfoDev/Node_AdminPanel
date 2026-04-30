64e152af8fcbe245d0f0ad555083ee26019acb72
Office Development <developmentoffice71@gmail.com>
2026-04-30 18:35:32 +0530

    modified:   Controller/backupMoveController.js

--- Controller/backupMoveController.js
+++ Controller/backupMoveController.js
@@ -272,7 +272,7 @@ const backupToDrive = async (req, res) => {
                 }
 
                 // Define the directory and file path for saving the file
-                localDir = path.join("..", "..", "downloads", fileName);
+                localDir = path.join("..", "..", "downloads");
                 bakPath = path.join(localDir, fileName);
                 fs.promises.mkdir(localDir, { recursive: true })
                     .then(() => {
