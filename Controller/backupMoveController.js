7bae389aa777da3bfac81c00e1086e205bebd5e8
Office Development <developmentoffice71@gmail.com>
2026-04-30 16:06:10 +0530

    modified:   Controller/backupMoveController.js 	modified:   Routes/userDashboardRoutes.js

--- Controller/backupMoveController.js
+++ Controller/backupMoveController.js
@@ -246,7 +246,7 @@ const backupToDrive = async (req, res) => {
 
         /* ========= DOWNLOAD ========= */
         // const localDir = path.join("/tmp", "downloads", fileName);
-        const localDir = path.join("..", "/downloads");
+        const localDir = path.join("..", "..", "..", '/tmp', "downloads", fileName);
 
         await fs.promises.mkdir(localDir, { recursive: true });
 
